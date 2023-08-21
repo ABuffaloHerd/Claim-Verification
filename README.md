6d0699cd27e73d0361d31e7ac1b116d2ecb1c635
ABuffaloHerd <blaster140902@gmail.com>
2023-08-21 17:46:13 +1200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Claim Verification Phase 2
+
+## A note about pytorch
+Pytorch works with your GPU only if it is CUDA supported. Otherwise it will use your CPU.
+
---
+++ pickle_jar.py
@@ -0,0 +1,64 @@
+"""
+Aidan Chan 20114106
+COMP702 RND - Claim Verification
+
+This file takes in a dataset and pickles it into a jar. Just kidding.
+This file takes in a dataset, tokenizes it for BERT and pickles it into a jar (pkl file).
+"""
+
+import pickle
+import pandas as pd
+import transformers
+import torch
+import os
+
+from transformers import BertTokenizer, BertModel, BertForSequenceClassification
+
+"""
+Headers in the dataset:
+Claim_id, Claim, Label (TFN) Evidence_id, Evidence, Article_id, Reason
+"""
+
+frames = []
+
+# Load the dataset
+for filename in os.listdir("data"):
+    if filename.endswith(".xlsx"):
+        df = pd.read_excel("data/" + filename, header=0)
+        print("Loaded dataset: " + filename)
+        frames.append(df)
+
+# Compile the dataset into three arrays (claim, evidence, label)
+claim = []
+evidence = []
+label = []
+
+for frame in frames:
+    claim.extend(frame['Claim'].tolist())
+    evidence.extend(frame['Evidence'].tolist())
+    label.extend(frame['Label (TFN)'].tolist())
+
+# Convert the label to a binary value
+label_map = {'T': 1, 'F': 0, 'N': 2}
+for i in range(len(label)):
+    label[i] = label_map[label[i]]
+
+# Tokenize the dataset
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+# Tokenize the claim and evidence
+print("Tokenizing the dataset...")
+print("DEBUG: Claim: " + claim[0])
+print("DEBUG: Evidence: " + evidence[0])
+
+print("DEBUG: " + str(len(claim)) + " claims and " + str(len(evidence)) + " evidence")
+
+# For some reason, one of these isn't a string. Convert them all to strings.
+claim = [str(c) for c in claim]
+evidence = [str(e) for e in evidence]
+
+encodings = tokenizer(claim, evidence, truncation=True, padding=True, max_length=512, return_tensors='pt')
+
+# Save the tokenized dataset to a pickle file
+with open('data.pkl', 'wb') as f:
+    pickle.dump(encodings, f)
